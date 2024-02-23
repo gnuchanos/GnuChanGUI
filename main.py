ab574a3f63f1c6ae4ef24cefb0da6d1ca3652394
GnuChanOS <117280480+gnuchanos@users.noreply.github.com>
2024-02-23 15:39:48 +0200

    little example

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ fun it's a serious goal of the project. if we're not having fun while making stu
 
 
 if __name__ == "__main__":
-    gc = GnuChanGUI(Title="", Size=(300, 200), resizable=False, finalize=True)
+    gc = GnuChanGUI(Title="", Size=(250, 600), resizable=False, finalize=True)
     Themecolors().GnuChanOS
 
-    layout = [ [gc.GText(title="test", xStretch=True, position="center")] ]
+    layout = [ [gc.GText(value="text", xStretch=True, position="center")],
+               [gc.GInput(value="user_input", xStretch=True)],
+               [gc.GButton(title="change text", font="Sans, 20", xStretch=True)],
+               [gc.GListBox(value="listbox", xStretch=True, yStretch=True, position="center")]
+               ]
 
     gc.GWindow(mainWindow=layout)
 
+    listboxList = []
     def update():
-        pass
+        global listboxList
+
+        if gc.event == "change text":
+            gc.window["text"].update(gc.GetValues["user_input"])
+            gc.window["user_input"].update("")
+
+            listboxList.append(gc.GetValues["user_input"])
+            gc.window["listbox"].update(listboxList)
 
     gc.update(GUpdate=update)
---
+++ makefile
@@ -0,0 +1,4 @@
+setup:
+	pip install .
+clean:
+	rm -r GnuChanGUI.egg-info build
